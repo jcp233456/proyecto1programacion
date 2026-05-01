a041414556bee1f8a11e74419b2663dae6a73a6e
carlos peñalonzo <juancarlospenalonzochiguil1@gmail.com>
2026-05-01 11:47:04 -0600

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,30 +30,30 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl2 = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBoxCodigo = new System.Windows.Forms.TextBox();
-            this.textBoxNombre = new System.Windows.Forms.TextBox();
-            this.textBoxMarca = new System.Windows.Forms.TextBox();
-            this.numeriPrecioCompra = new System.Windows.Forms.NumericUpDown();
-            this.numericPrecioVenta = new System.Windows.Forms.NumericUpDown();
-            this.numericCantidad = new System.Windows.Forms.NumericUpDown();
             this.buttomIngresoProducto = new System.Windows.Forms.Button();
+            this.numericCantidad = new System.Windows.Forms.NumericUpDown();
+            this.numericPrecioVenta = new System.Windows.Forms.NumericUpDown();
+            this.numeriPrecioCompra = new System.Windows.Forms.NumericUpDown();
+            this.textBoxMarca = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxCodigo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabControl2.SuspendLayout();
             this.tabPage3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numeriPrecioCompra)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericPrecioVenta)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPrecioVenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeriPrecioCompra)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -76,16 +76,6 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "admin";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1041, 369);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabControl2
             // 
@@ -120,60 +110,58 @@
             this.tabPage3.Text = "agregar{";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // tabPage4
+            // buttomIngresoProducto
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 25);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(981, 291);
-            this.tabPage4.TabIndex = 1;
-            this.tabPage4.Text = "editar";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            this.buttomIngresoProducto.Location = new System.Drawing.Point(606, 151);
+            this.buttomIngresoProducto.Name = "buttomIngresoProducto";
+            this.buttomIngresoProducto.Size = new System.Drawing.Size(241, 63);
+            this.buttomIngresoProducto.TabIndex = 12;
+            this.buttomIngresoProducto.Text = "Ingresar Producto";
+            this.buttomIngresoProducto.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // numericCantidad
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Codigo producto";
+            this.numericCantidad.Location = new System.Drawing.Point(261, 318);
+            this.numericCantidad.Name = "numericCantidad";
+            this.numericCantidad.Size = new System.Drawing.Size(147, 22);
+            this.numericCantidad.TabIndex = 11;
             // 
-            // label2
+            // numericPrecioVenta
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 80);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nombre producto";
+            this.numericPrecioVenta.DecimalPlaces = 2;
+            this.numericPrecioVenta.Location = new System.Drawing.Point(261, 255);
+            this.numericPrecioVenta.Name = "numericPrecioVenta";
+            this.numericPrecioVenta.Size = new System.Drawing.Size(147, 22);
+            this.numericPrecioVenta.TabIndex = 10;
             // 
-            // label3
+            // numeriPrecioCompra
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 135);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Marca producto";
+            this.numeriPrecioCompra.DecimalPlaces = 2;
+            this.numeriPrecioCompra.Location = new System.Drawing.Point(261, 196);
+            this.numeriPrecioCompra.Name = "numeriPrecioCompra";
+            this.numeriPrecioCompra.Size = new System.Drawing.Size(147, 22);
+            this.numeriPrecioCompra.TabIndex = 9;
             // 
-            // label4
+            // textBoxMarca
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 198);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(114, 16);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Precio de compra";
+            this.textBoxMarca.Location = new System.Drawing.Point(266, 135);
+            this.textBoxMarca.Name = "textBoxMarca";
+            this.textBoxMarca.Size = new System.Drawing.Size(142, 22);
+            this.textBoxMarca.TabIndex = 8;
             // 
-            // label5
+            // textBoxNombre
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 255);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 16);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Precio de venta";
+            this.textBoxNombre.Location = new System.Drawing.Point(266, 80);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(142, 22);
+            this.textBoxNombre.TabIndex = 7;
+            // 
+            // textBoxCodigo
+            // 
+            this.textBoxCodigo.Location = new System.Drawing.Point(266, 25);
+            this.textBoxCodigo.Name = "textBoxCodigo";
+            this.textBoxCodigo.Size = new System.Drawing.Size(142, 22);
+            this.textBoxCodigo.TabIndex = 6;
             // 
             // label6
             // 
@@ -184,58 +172,70 @@
             this.label6.TabIndex = 5;
             this.label6.Text = "Cantidad en existencia";
             // 
-            // textBoxCodigo
+            // label5
             // 
-            this.textBoxCodigo.Location = new System.Drawing.Point(266, 25);
-            this.textBoxCodigo.Name = "textBoxCodigo";
-            this.textBoxCodigo.Size = new System.Drawing.Size(142, 22);
-            this.textBoxCodigo.TabIndex = 6;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio de venta";
             // 
-            // textBoxNombre
+            // label4
             // 
-            this.textBoxNombre.Location = new System.Drawing.Point(266, 80);
-            this.textBoxNombre.Name = "textBoxNombre";
-            this.textBoxNombre.Size = new System.Drawing.Size(142, 22);
-            this.textBoxNombre.TabIndex = 7;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio de compra";
             // 
-            // textBoxMarca
+            // label3
             // 
-            this.textBoxMarca.Location = new System.Drawing.Point(266, 135);
-            this.textBoxMarca.Name = "textBoxMarca";
-            this.textBoxMarca.Size = new System.Drawing.Size(142, 22);
-            this.textBoxMarca.TabIndex = 8;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Marca producto";
             // 
-            // numeriPrecioCompra
+            // label2
             // 
-            this.numeriPrecioCompra.DecimalPlaces = 2;
-            this.numeriPrecioCompra.Location = new System.Drawing.Point(261, 196);
-            this.numeriPrecioCompra.Name = "numeriPrecioCompra";
-            this.numeriPrecioCompra.Size = new System.Drawing.Size(147, 22);
-            this.numeriPrecioCompra.TabIndex = 9;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre producto";
             // 
-            // numericPrecioVenta
+            // label1
             // 
-            this.numericPrecioVenta.DecimalPlaces = 2;
-            this.numericPrecioVenta.Location = new System.Drawing.Point(261, 255);
-            this.numericPrecioVenta.Name = "numericPrecioVenta";
-            this.numericPrecioVenta.Size = new System.Drawing.Size(147, 22);
-            this.numericPrecioVenta.TabIndex = 10;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo producto";
             // 
-            // numericCantidad
+            // tabPage4
             // 
-            this.numericCantidad.Location = new System.Drawing.Point(261, 318);
-            this.numericCantidad.Name = "numericCantidad";
-            this.numericCantidad.Size = new System.Drawing.Size(147, 22);
-            this.numericCantidad.TabIndex = 11;
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(981, 379);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "editar";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // buttomIngresoProducto
+            // tabPage2
             // 
-            this.buttomIngresoProducto.Location = new System.Drawing.Point(606, 151);
-            this.buttomIngresoProducto.Name = "buttomIngresoProducto";
-            this.buttomIngresoProducto.Size = new System.Drawing.Size(241, 63);
-            this.buttomIngresoProducto.TabIndex = 12;
-            this.buttomIngresoProducto.Text = "button1";
-            this.buttomIngresoProducto.UseVisualStyleBackColor = true;
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1041, 448);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -250,9 +250,9 @@
             this.tabControl2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numeriPrecioCompra)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericPrecioVenta)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPrecioVenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeriPrecioCompra)).EndInit();
             this.ResumeLayout(false);
 
         }
